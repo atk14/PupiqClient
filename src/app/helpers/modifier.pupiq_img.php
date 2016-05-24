832d9eb20ba1b1bc00fb8b2c4770f153b6751cf0
Michal Vojan <michal.vojan@gmail.com>
2016-05-24 17:46:08 +0200

    helper pupiq_img accepts attributes

--- src/app/helpers/modifier.pupiq_img.php
+++ src/app/helpers/modifier.pupiq_img.php
@@ -1,13 +1,42 @@
 <?php
 /**
- * Vice info na https://secure.orbit41.com:441/en/wiki/pupiq/
+ * Smarty modifier to render img tag using image from Pupiq
+ * More info na https://secure.orbit41.com:441/en/wiki/pupiq/
+ * @package Pupiq
+ * @package Helpers
  */
-function smarty_modifier_pupiq_img($url,$geometry = "100",$user = null){
+
+/**
+ * Smarty modifier to render img tag using image from Pupiq
+ * More info at https://secure.orbit41.com:441/en/wiki/pupiq/
+ *
+ * @param string $url
+ * @param string $geometry
+ * @param ApiUser $user
+ * @param string $attrs
+ */
+function smarty_modifier_pupiq_img($url,$geometry = "100",$user = null,$attrs=null){
 	if(!$url){ return; }
 	if(is_object($url)){
 		$url = $url->getUrl();
 	}
 	$api_key = isset($user) ? $user->getApiKey() : null;
+	$attrsAr = array();
+
+	# muzeme poslat dalsi atributy jako retezec => "class='image image-warning'|title='Warning sign'"
+	if ($attrs=trim($attrs)) {
+		if (is_string($attrs)) {
+			$_attrs = preg_split("/,/",$attrs);
+			array_walk($_attrs, function($v,$k) use (&$attrsAr) {
+				list($k,$v) = preg_split("/=/", $v);
+				# remove quotes from value
+				if (preg_match("/^[\"'](.+)[\"']$/", $v, $m)) {
+					$v = $m[1];
+				}
+				$attrsAr[$k]=$v;
+			});
+		}
+	}
 	$p = new Pupiq($url,$api_key);
-	return $p->getImgTag($geometry);
+	return $p->getImgTag($geometry, array("attrs" => $attrsAr));
 }
