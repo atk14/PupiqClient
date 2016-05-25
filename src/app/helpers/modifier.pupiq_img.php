c1c2a046a6b95ae42c0a95518f3bec0ea6084549
Michal Vojan <michal.vojan@gmail.com>
2016-05-25 09:19:11 +0200

    pupiq_img helper test

--- src/app/helpers/modifier.pupiq_img.php
+++ src/app/helpers/modifier.pupiq_img.php
@@ -2,6 +2,7 @@
 /**
  * Smarty modifier to render img tag using image from Pupiq
  * More info at https://github.com/atk14/PupiqClient
+ *
  * @package Pupiq
  * @package Helpers
  */
@@ -11,20 +12,21 @@
  * More info at https://github.com/atk14/PupiqClient#usage-in-templates
  *
  * @param string $url
- * @param string $geometry
- * @param string $attrs
+ * @param string $geometry parameters for Pupiq service with information what to do with the image
+ * @param string $attrs additional attributes for img tag
  */
 function smarty_modifier_pupiq_img($url,$geometry = "100",$attrs=null){
 	if(!$url){ return; }
 	if(is_object($url)){
 		$url = $url->getUrl();
 	}
-	$attrsAr = array();
 
 	# muzeme poslat dalsi atributy jako retezec => "class='image image-warning',title='Warning sign'"
-	if ($attrs=trim($attrs)) {
+	# we can pass 
+	$attrsAr = array();
+	if ($attrs) {
 		if (is_string($attrs)) {
-			$_attrs = preg_split("/,/",$attrs);
+			$_attrs = preg_split("/,/",trim($attrs));
 			array_walk($_attrs, function($v,$k) use (&$attrsAr) {
 				list($k,$v) = preg_split("/=/", $v);
 				$k = trim($k);
@@ -35,6 +37,8 @@ function smarty_modifier_pupiq_img($url,$geometry = "100",$attrs=null){
 				}
 				$attrsAr[$k]=$v;
 			});
+		} else {
+			$attrsAr = $attrs;
 		}
 	}
 	$p = new Pupiq($url);
