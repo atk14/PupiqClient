0bc3d3a463fd1b7e1af88e251497790dbd66305b
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-04-08 14:57:33 +0200

    Version 1.7

--- src/lib/pupiq.php
+++ src/lib/pupiq.php
@@ -9,7 +9,7 @@ defined("PUPIQ_DEFAULT_WATERMARK_DEFINITION") || define("PUPIQ_DEFAULT_WATERMARK
 
 class Pupiq {
 
-	const VERSION = "1.4.2";
+	const VERSION = "1.7";
 
 	protected $_api_key = "";
 
