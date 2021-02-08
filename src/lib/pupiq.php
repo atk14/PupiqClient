265adef82b038a6a738e08c45a9776e2bdfad4b5
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-02-08 15:23:00 +0100

    Version 1.13

--- src/lib/pupiq.php
+++ src/lib/pupiq.php
@@ -9,7 +9,7 @@ defined("PUPIQ_DEFAULT_WATERMARK_DEFINITION") || define("PUPIQ_DEFAULT_WATERMARK
 
 class Pupiq {
 
-	const VERSION = "1.12.4";
+	const VERSION = "1.13";
 
 	protected $_api_key = "";
 
