f7b7a5c6dff6e3530a050d1fd33341fc8e201e0b
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-10-20 13:02:33 +0200

    Testing

--- test/tc_pupiq_attachment.php
+++ test/tc_pupiq_attachment.php
@@ -3,12 +3,14 @@ class TcPupiqAttachment extends TcBase{
 
 	function test(){
 		$attachment = new PupiqAttachment("http://i.pupiq.net/a/c/c/2a1/f2a1/15257/user%20manual.pdf");
-
 		$this->assertEquals("user manual.pdf",$attachment->getFilename());
 		$this->assertEquals("15257",$attachment->getFilesize());
 		$this->assertEquals("pdf",$attachment->getSuffix());
 		$this->assertEquals("application/pdf",$attachment->getMimeType());
 		$this->assertEquals(62113,$attachment->getAttachmentId());
 
+
+		$attachment = new PupiqAttachment("http://i.pupiq.net/a/65/65/cf4/cf4/2358/sample.webp");
+		$this->assertEquals("image/webp",$attachment->getMimeType());
 	}
 }
