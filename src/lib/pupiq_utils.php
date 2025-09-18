0de0ea4362166d7f82c5a442f08a160369cbee6c
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-09-18 16:39:36 +0200

    PupiqUtils::DecodeParams() improved - now considering escaping of comma and equals symbol

--- src/lib/pupiq_utils.php
+++ src/lib/pupiq_utils.php
@@ -5,17 +5,20 @@ class PupiqUtils {
 	 * $params = PupiqUtils::DecodeParams('alt=Image,title="Very nice image",data-clickable'); // ["alt" => "Image", "title" => "Very nice image", "data-clickable" => true]
 	 */
 	static function DecodeParams($params){
-		if(!$params){
-			return array();
-		}
+		static $placeholder;
+		if(!$placeholder){ $placeholder = "~X".uniqid()."X~"; }
 
-		if(is_array($params)){
-			return $params;
-		}
+		if(!$params){ return array(); }
+		if(is_array($params)){ return $params; }
 
 		$out = array();
 
+		$params = strtr($params,array(
+			"\\," => "$placeholder-cm",
+			"\\=" => "$placeholder-eq",
+		));
 		$_attrs = preg_split("/,/",trim($params));
+
 		array_walk($_attrs, function($v,$k) use (&$out) {
 			$ary = preg_split('/=/',$v);
 			if(sizeof($ary)==1){
@@ -27,11 +30,15 @@ class PupiqUtils {
 				$v = join('=',$ary);
 				$k = trim($k);
 				$v = trim($v);
+				$v = strtr($v,array(
+					"$placeholder-cm" => ",",
+					"$placeholder-eq" => "=",
+				));
 				# remove quotes from value
 				if (preg_match("/^[\"'](.*)[\"']$/", $v, $m)) {
 					$v = trim($m[1]);
 				}
-				$out[$k]=$v;
+				$out[$k] = $v;
 			}
 		});
 
